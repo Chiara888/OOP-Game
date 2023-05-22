09ea87e205e0df1d2bb722a4b8ff80af30fef7b2
Chiara888 <carolin@family-klose.de>
2023-05-22 11:18:53 +0200

    implemented randomly appearing obstacles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 class Player {
     constructor() {
-        this.width = 8;
-        this.height = 10;
+        this.width = 4;
+        this.height = 8;
         this.positionX = 50 - this.width/2;
         this.positionY = 50;
 
@@ -42,9 +42,79 @@ class Player {
         this.domElement.style.bottom = this.positionY +"vh"
     }
 
-    shootObstacles () {
-    }
+    // shootObstacles () {
+    // }
 }
 
+
+class Obstacle {
+    constructor () {
+        this.width = 2;
+        this.height = 2;
+        this.positionX = 50 - this.width/2;
+        this.positionY = 80;
+
+        this.domElement = null;
+        this.createDomElement();
+    }
+
+    createDomElement () {
+        this.domElement = document.createElement("div");
+
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
+
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.domElement);
+    }
+    
+    randomPosition (min, max) {
+        return Math.floor(Math.random() * (max-min)+min); 
+    }
+
+    appearRandom () {
+        this.domElement.style.left = this.randomPosition(20,98) + "vw";
+        this.domElement.style.bottom = this.randomPosition(20,98) + "vh";
+    }
+}
+ 
+// Commands general 
 const player = new Player();
-player.createDomElement();
+const obstacleArr = [];
+
+
+// Commands for the player
+document.addEventListener("keydown", (event) => {
+    if (event.code === "ArrowLeft") {
+        player.moveLeft();
+    } else if (event.code === "ArrowRight") {
+        player.moveRight();
+    } else if (event.code === "ArrowDown") {
+        player.moveBackwards();
+    } else if (event.code === "ArrowUp") {
+        player.moveForward();
+    }
+})
+
+// Commands for the obstacles
+
+setInterval(() => {
+    const createObstacle = new Obstacle();
+    obstacleArr.push(createObstacle);
+    obstacleArr.forEach((obstacleElm) => {
+        obstacleElm.appearRandom();
+    })}, 4000); 
+
+
+setInterval(() => {
+    const obstacleone = obstacleArr.shift();
+    obstacleone.domElement.remove();
+}, 5000);
+
+
+
+
+
